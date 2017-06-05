ea693cab67f293158cf1c87b9f5e9521c9b6db7b
slavaGanzin <slava.ganzin@gmail.com>
2017-06-05 22:39:28 +0300

    recursive tparent

--- lua/tparents.lua
+++ lua/tparents.lua
@@ -1,1 +1,14 @@
-return redis.call('smembers', key .. '::P')
+local function parents(id)
+  local p = {}
+  for _, v in ipairs(redis.call('smembers', prefix .. id  .. '::P')) do
+    table.insert(p,v)
+    table.insert(p, parents(v))
+  end
+  return p
+end
+
+if ARGV[2] == 'ALL' then
+  return parents(id)
+else
+  return redis.call('smembers', key .. '::P')
+end
